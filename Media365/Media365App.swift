8fce2c8c989e7800c9503fd43c90b992ce8582c2
rezaulislamtarek <mrezaulislam.t@gmail.com>
2023-07-06 13:54:26 +0600

    Initial Commit

---
+++ Media365/Media365App.swift
@@ -0,0 +1,17 @@
+//
+//  Media365App.swift
+//  Media365
+//
+//  Created by Rezaul Islam on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Media365App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
