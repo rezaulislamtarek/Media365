7803e4bbc5546d53952fdcc6815fcdd2abd160b9
rezaulislamtarek <mrezaulislam.t@gmail.com>
2023-07-06 19:24:51 +0600

    background colour list

--- Media365/ContentView.swift
+++ Media365/ContentView.swift
@@ -6,14 +6,12 @@
 //
 
 import SwiftUI
+import DevRh
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+             DevListView()
         }
         .padding()
     }
